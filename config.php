55432d5f70f4eab1d7f4d1cb22794c3fd0f2a684
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2024-02-12 00:37:13 +0700

    Update config.php
    update

--- config.php
+++ config.php
@@ -6,8 +6,5 @@ define('POSITION_URL', PATH . '?page=' . $page);
 define('RESOUCES', 'https://rahman115.github.io/argajaladri.or.id/resource/');
 
 // this configuration for database website
-define('DB_HOST', 'ngupasan.idweb.host'); // host yang di gunakan
-define('DB_USERNAME', 'argajal3_media');
-define('DB_PASSWORD', 'Abudurahman#93@');
-define('DB_NAME', 'argajal3_media'); // database yang di gunakan
+
 $db = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME) or die ("Ada Masalah Di Koneksi! - DATABASE");
