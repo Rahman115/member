ec0e23b91020d996838df5379eabe4a2533d1426
Abdur Rahman <abdurrahmanariyansyah@gmail.com>
2024-02-12 02:53:04 +0700

    Update config.php
    update

--- config.php
+++ config.php
@@ -1,11 +1,6 @@
 <?php
 
 
-define('DB_HOST', 'ngupasan.idweb.host'); // host yang di gunakan
-define('DB_USERNAME', 'argajal3_media');
-define('DB_PASSWORD', 'Abudurahman#93@');
-define('DB_NAME', 'argajal3_media'); // database yang di gunakan
 
-$db = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME) or die ("Ada Masalah Di Koneksi! - DATABASE");
 
 ?>
